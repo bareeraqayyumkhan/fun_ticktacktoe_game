589a62c0a8718fea4c844e525e395ce3cad7e9a7
bareeraqayyumkhan <bareeraq5@gmail.com>
2026-04-09 14:15:21 +0500

    Update ticktacktoe.cpp

--- ticktacktoe.cpp
+++ ticktacktoe.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    char b1 = '1', b2 = '2', b3 = '3', b4 = '4', b5 = '5', b6 = '6', b7 = '7', b8 = '8', b9 = '9';
+    char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
     int turn = 1;
     char mark;
     int choice;
@@ -13,15 +13,20 @@ int main() {
         cout << "***********************" << endl;
         cout << "* TIC TAC TOE      *" << endl;
         cout << "***********************" << endl;
-        cout << "     " << b1 << " | " << b2 << " | " << b3 << endl;
-        cout << "    -----------" << endl;
-        cout << "     " << b4 << " | " << b5 << " | " << b6 << endl;
-        cout << "    -----------" << endl;
-        cout << "     " << b7 << " | " << b8 << " | " << b9 << endl;
+
+        for (int i = 0; i < 3; i++) {
+            cout << "     ";
+            for (int j = 0; j < 3; j++) {
+                cout << board[i][j];
+                if (j < 2) cout << " | ";
+            }
+            cout << endl;
+            if (i < 2) cout << "    -----------" << endl;
+        }
         cout << "***********************" << endl;
 
         if (turn % 2 != 0) {
-            mark = 'X'; 
+            mark = 'X';
             cout << "Player 1 (X), enter box: ";
         } else {
             mark = 'O';
@@ -30,41 +35,46 @@ int main() {
 
         cin >> choice;
 
-        if (choice == 1 && b1 == '1') b1 = mark;
-        else if (choice == 2 && b2 == '2') b2 = mark;
-        else if (choice == 3 && b3 == '3') b3 = mark;
-        else if (choice == 4 && b4 == '4') b4 = mark;
-        else if (choice == 5 && b5 == '5') b5 = mark;
-        else if (choice == 6 && b6 == '6') b6 = mark;
-        else if (choice == 7 && b7 == '7') b7 = mark;
-        else if (choice == 8 && b8 == '8') b8 = mark;
-        else if (choice == 9 && b9 == '9') b9 = mark;
-        else {
+        int row = (choice - 1) / 3;
+        int col = (choice - 1) % 3;
+
+        if (choice >= 1 && choice <= 9 && board[row][col] != 'X' && board[row][col] != 'O') {
+            board[row][col] = mark;
+        } else {
             cout << "Invalid move! Try again." << endl;
             continue;
         }
 
-        if ((b1 == b2 && b2 == b3) || (b4 == b5 && b5 == b6) || (b7 == b8 && b8 == b9) ||
-            (b1 == b4 && b4 == b7) || (b2 == b5 && b5 == b8) || (b3 == b6 && b6 == b9) ||
-            (b1 == b5 && b5 == b9) || (b3 == b5 && b5 == b7)) {
-            win = true;
-        } else {
-            turn++;
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == board[i][1] && board[i][1] == board[i][2]) ||
+                (board[0][i] == board[1][i] && board[1][i] == board[2][i])) {
+                win = true;
+            }
         }
+        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+            (board[0][2] == board[1][1] && board[1][1] == board[2][0])) {
+            win = true;
+        }
+
+        if (!win) turn++;
     }
 
-    cout << "     " << b1 << " | " << b2 << " | " << b3 << endl;
-    cout << "    -----------" << endl;
-    cout << "     " << b4 << " | " << b5 << " | " << b6 << endl;
-    cout << "    -----------" << endl;
-    cout << "     " << b7 << " | " << b8 << " | " << b9 << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "     ";
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        cout << endl;
+        if (i < 2) cout << "    -----------" << endl;
+    }
 
     if (win) {
         if (turn % 2 != 0) cout << "⋆˚꩜｡ PLAYER 1 WINS, WELL DONE DIVA!!⋆˚꩜｡" << endl;
-        else cout << " ᯓ★ PLAYER 2 WINS, WELL DONE DIVA!!!ᯓ★"<< endl;
+        else cout << " ᯓ★ PLAYER 2 WINS, WELL DONE DIVA!!!ᯓ★" << endl;
     } else {
-        cout << "IT'S A DRAW " << endl;
-        cout << "Oh look at that!That was a high-speed chase to a brick wall LOL." <<endl; 
+        cout << "IT'S A DRAW" << endl;
+        cout << "Oh look at that! That was a high-speed chase to a brick wall LOL." << endl;
     }
 
     return 0;
